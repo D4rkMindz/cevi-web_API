1e624453ce7012c15d712a99957074a9f043cc85
bjoernpfoster <bjoern.pfoster@gmail.com>
2017-11-17 10:36:09 +0100

    Renamed renderEngine to Engine's classname and added DocBlocks

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -3,18 +3,33 @@
 namespace App\Controller;
 
 use App\Table\UserTable;
+use League\Plates\Engine;
 use Slim\Container;
 
+/**
+ * Class IndexController
+ */
 class IndexController extends AppController
 ***REMOVED***
     private $connection;
 
+    /**
+     * IndexController constructor.
+     *
+     * @param Container $container
+     */
     public function __construct(Container $container)
     ***REMOVED***
         $this->connection = $container->get('connection');
-        parent::__construct($container->get('renderEngine'));
+        parent::__construct($container->get(Engine::class));
 ***REMOVED***
 
+    /**
+     * Index method,
+     * .
+     *
+     * @return string
+     */
     public function index()
     ***REMOVED***
         $userTable = new UserTable();
@@ -23,6 +38,7 @@ class IndexController extends AppController
             'page' => 'Home',
             'userData' => $userData,
         ];
+
         return $this->render('view::index.html.php', $viewData);
 ***REMOVED***
 ***REMOVED***
