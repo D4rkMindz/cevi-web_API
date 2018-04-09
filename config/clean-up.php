9a02f2607f34bc4894ca1afa52374fff2051acea
Björn Pfoster <bjoern.pfoster@gmail.com>
2018-04-09 20:08:10 +0200

    updated deployment scripts

--- config/clean-up.php
+++ config/clean-up.php
@@ -1,63 +1,63 @@
 <?php
-$backupDirectory = scandir('./backup/');
-$homeDirectory = scandir('.');
+$backupDirectory = scandir("./backup/");
+$homeDirectory = scandir(".");
 foreach ($homeDirectory as $dir)***REMOVED***
-    if (is_dir($dir) && strpos($dir, 'htdocs_') !== false)***REMOVED***
-        echo 'Removig $dir \n';
-        system('rm -rf $dir');
+    if (is_dir($dir) && strpos($dir, "htdocs_") !== false)***REMOVED***
+        echo "Removig $dir \n";
+        system("rm -rf $dir");
 ***REMOVED***
 ***REMOVED***
 if (!isset($argv[1])) ***REMOVED***
     $argv[1] = 0;
 ***REMOVED***
-$time = date('Y-m-d_H-i-s', time() - $argv[1]);
+$time = date("Y-m-d_H-i-s", time() - $argv[1]);
 $directories = [];
 $directoriesToDelete = [];
 foreach ($backupDirectory as $dir) ***REMOVED***
     if (is_dir($dir)) ***REMOVED***
-        if ($dir < $time && strpos($dir, '.') === false) ***REMOVED***
+        if ($dir < $time && strpos($dir, ".") === false) ***REMOVED***
             $directoriesToDelete[] = $dir;
     ***REMOVED***
 ***REMOVED***
 ***REMOVED***
 if ($argv[1] <= 60) ***REMOVED***
-    $t = 'Seconds';
+    $t = "Seconds";
     $val = $argv[1];
 ***REMOVED*** elseif ($argv[1] <= 3600) ***REMOVED***
-    $t = 'Minutes';
+    $t = "Minutes";
     $val = $argv[1] / 60;
 ***REMOVED*** elseif ($argv[1] <= 86400) ***REMOVED***
-    $t = 'Hours';
+    $t = "Hours";
     $val = ($argv[1] / 60) / 60;
 ***REMOVED*** elseif ($argv[1] <= 31536000) ***REMOVED***
-    $t = 'Days';
+    $t = "Days";
     $val = (($argv[1] / 60) / 60) / 24;
 ***REMOVED*** elseif ($argv[1] > 31536000) ***REMOVED***
-    $t = 'Years';
+    $t = "Years";
     $val = ((($argv[1] / 60) / 60) / 24) / 365;
 ***REMOVED***
 foreach ($directoriesToDelete as $dir) ***REMOVED***
-    echo 'Deleting directory $dir (older than $val $t)\n';
-    system('rm -rf $dir');
+    echo "Deleting directory $dir (older than $val $t)\n";
+    system("rm -rf $dir");
 ***REMOVED***
-$dirContent= scandir('.');
+$dirContent= scandir(".");
 $files = [];
-if (!is_dir('backup/'))***REMOVED***
-    echo 'Creating ./backup directory\n';
-    system('mkdir ./backup/');
+if (!is_dir("backup/"))***REMOVED***
+    echo "Creating ./backup directory\n";
+    system("mkdir ./backup/");
 ***REMOVED***
 foreach ($dirContent as $file)***REMOVED***
-    if (is_file($file) && pathinfo($file, PATHINFO_EXTENSION) == 'zip')***REMOVED***
-        $fulldate = str_replace('contact_form_', '', $file);
+    if (is_file($file) && pathinfo($file, PATHINFO_EXTENSION) == "zip")***REMOVED***
+        $fulldate = str_replace("contact_form_", "", $file);
         $date = substr($fulldate, 0, -13);
         $files[] = $file;
-        if (!is_dir('./backup/$date/'))***REMOVED***
-            echo '\n';
-            echo 'Creating ./backup/$date directory\n';
-            system('mkdir ./backup/$date/');
+        if (!is_dir("./backup/$date/"))***REMOVED***
+            echo "\n";
+            echo "Creating ./backup/$date directory\n";
+            system("mkdir ./backup/$date/");
     ***REMOVED***
-        echo 'Moving $file to backup/$date/$file\n';
-        rename($file, 'backup/$date/$file');
+        echo "Moving $file to backup/$date/$file\n";
+        rename($file, "backup/$date/$file");
 ***REMOVED***
 ***REMOVED***
-echo '\nFinished cleaning up zip files\n';
+echo "\nFinished cleaning up zip files\n";
--- config/deploy.php
+++ config/deploy.php
@@ -1,40 +1,43 @@
 <?php
-date_default_timezone_set('Europe/Berlin');
-$time = date('Y-m-d_H-i-s', time());
-if (is_dir('/release/')) ***REMOVED***
-    echo 'Removing directory ./release/';
-    system('rmdir ./release/');
+date_default_timezone_set("Europe/Berlin");
+$time = date("Y-m-d_H-i-s");
+if (is_dir("/release/")) ***REMOVED***
+    echo "Removing directory ./release/";
+    system("rmdir ./release/");
 ***REMOVED***
-echo 'Creating directory ./release/\n';
-system('mkdir ./release/');
-echo 'Unzipping $argv[1]\n';
-system('unzip $argv[1] -d ./release/');
-if (is_dir('./htdocs/'))***REMOVED***
-    echo 'Renaming ./htdocs/ to ./htdocs_$time\n';
-    system('mv ./htdocs/ ./htdocs_$time');
+
+echo "Creating directory ./release/\n";
+system("mkdir ./release/");
+
+echo "Unzipping $argv[1]\n";
+system("unzip $argv[1] -d ./release/");
+
+if (is_dir("./htdocs/"))***REMOVED***
+    echo "Renaming ./htdocs/ to ./htdocs_$time\n";
+    system("mv ./htdocs/ ./htdocs_$time");
 ***REMOVED***
-echo 'Renaming ./release/ to ./htdocs/\n';
-system('mv ./release/ ./htdocs/');
-echo 'Removing zipfile $argv[1]\n';
-system('rm $argv[1] -rf');
-if (!is_dir('./htdocs/tmp/logs')) ***REMOVED***
-    echo 'Creating /logs directory';
-    system('mkdir ./htdocs/tmp/logs');
+echo "Renaming ./release/ to ./htdocs/\n";
+system("mv ./release/ ./htdocs/");
+echo "Removing zipfile $argv[1]\n";
+system("rm $argv[1] -rf");
+if (!is_dir("./htdocs/tmp/logs")) ***REMOVED***
+    echo "Creating /logs directory";
+    system("mkdir ./htdocs/tmp/logs");
 ***REMOVED***
-if (!is_dir('./htdocs/tmp/cache')) ***REMOVED***
-    echo 'Creating /cache directory';
-    system('mkdir ./htdocs/tmp/cache');
+if (!is_dir("./htdocs/tmp/cache")) ***REMOVED***
+    echo "Creating /cache directory";
+    system("mkdir ./htdocs/tmp/cache");
 ***REMOVED***
-echo 'Updating directory permissions to 775\n';
-system('chmod -R 775 ./htdocs/tmp/');
-echo 'Updating permissions';
-system('chmod 775 ./htdocs/vendor/bin/phinx && chmod -R 775 ./htdocs/vendor/robmorgan/');
-echo 'Migrating database';
-system('cd htdocs/config/ && ../vendor/bin/phinx migrate');
-system('cd ..');
-echo 'Deleting old Backups ...';
-system('php clean-up.php 31536000');
-echo '\n';
-echo '--------------------------------------------------------\n';
-echo 'Server deployment done\n';
-echo '--------------------------------------------------------\n';
+echo "Updating directory permissions to 775\n";
+system("chmod -R 775 ./htdocs/tmp/");
+echo "Updating permissions";
+system("chmod 775 ./htdocs/vendor/bin/phinx && chmod -R 775 ./htdocs/vendor/robmorgan/");
+echo "Migrating database";
+system("cd htdocs/config/ && ../vendor/bin/phinx migrate");
+system("cd ..");
+echo "Deleting old Backups ...";
+system("php clean-up.php 31536000");
+echo "\n";
+echo "--------------------------------------------------------\n";
+echo "Server deployment done\n";
+echo "--------------------------------------------------------\n";
