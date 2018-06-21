a79fa89d43211c6f089bf8732ccb951c0a515a18
Björn <bjoern.pfoster@gmail.co>
2018-06-21 10:43:03 +0200

    updated config

--- config/environment.travis.php
+++ config/environment.travis.php
@@ -5,6 +5,10 @@ $config['db']['username'] = "root";
 $config['db']['password'] = "travis_passwd";
 $config['db']['port'] = 3306;
 $config['db']['host'] = 'localhost';
+$config['db']['mysql_executable'] = 'mysql';
+$config['db']['mysqldump_executable'] = 'mysqldump';
+$config['db']['host'] = 'localhost';
+
 $config['jwt']['active'] = true;
 //
 //$env['displayErrorDetails'] = true;
