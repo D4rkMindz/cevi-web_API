ede4c8748f0f74df024a9b35cf37b2103d92c012
Björn <bjoern.pfoster@gmail.co>
2018-06-21 10:19:52 +0200

    Update env config for travis

--- config/environment.travis.php
+++ config/environment.travis.php
@@ -1,6 +1,10 @@
 <?php
 $config = [];
 $config['db']['database'] = "cevi_web_test";
+$config['db']['username'] = "root";
+$config['db']['password'] = "travis_passwd";
+$config['db']['port'] = 3306;
+$config['db']['host'] = 'localhost';
 $config['jwt']['active'] = true;
 //
 //$env['displayErrorDetails'] = true;
